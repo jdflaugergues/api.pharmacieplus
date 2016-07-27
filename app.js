3effd24c238f242bee4fb6e811c795f863e3f23c
jdflaugergues <time_has_come_260@hotmail.com>
2016-07-27 21:10:02 +0200

    remove users route

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const express      = require('express'),
       sysInfo      = require('./utils/sys-info'),
       env          = process.env;
 
-// route users
-const users = require('./routes/users');
 
 const app = express();
 

2943712891f2b60826f45d656a6bf2a511a5faf8
jdflaugergues <time_has_come_260@hotmail.com>
2016-07-27 13:05:09 +0200

    update

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express      = require('express'),
+      http         = require('http'),
+      fs           = require('fs'),
+      path         = require('path'),
+      favicon      = require('serve-favicon'),
+      logger       = require('morgan'),
+      cookieParser = require('cookie-parser'),
+      bodyParser   = require('body-parser'),
+      debug        = require('debug')('pharmacieplus:app');
+      contentTypes = require('./utils/content-types'),
+      sysInfo      = require('./utils/sys-info'),
+      env          = process.env;
 
+
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -28,11 +37,44 @@ app.use('/', routes);
 app.use('/v1/pharmacies', pharmaciesRoutes); // Requêtes vers /pharmacies/*
 
 
+app.get('/health', function(req, res) {
+  res.writeHead(200);
+  res.end();
+});
+
+app.get('/info/gen', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-cache, no-store');
+  res.end(JSON.stringify(sysInfo[req.url.slice(6)]()));
+});
+
+app.get('/info/poll', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-cache, no-store');
+  res.end(JSON.stringify(sysInfo[req.url.slice(6)]()));
+});
+
+app.get('/home', function(req, res) {
+  fs.readFile('./static/' + 'index.html', function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+    } else {
+      let ext = path.extname(req.url).slice(1);
+      res.setHeader('Content-Type', contentTypes[ext]);
+      if (ext === 'html') {
+        res.setHeader('Cache-Control', 'no-cache, no-store');
+      }
+      res.end(data);
+    }
+  });
+});
+
 // catch 404 and forward to error handler
 app.use((request, response, next) => {
   var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+err.status = 404;
+next(err);
 });
 
 // error handlers
@@ -42,31 +84,34 @@ app.use((request, response, next) => {
 if (app.get('env') === 'development') {
   app.use((err, request, response, next) => {
     response.status(err.status || 500);
-    response.render('error', {
-      message: err.message,
-      error: err
-    });
+  response.render('error', {
+    message: err.message,
+    error: err
   });
+});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use((err, request, response, next) => {
   response.status(err.status || 500);
-  response.render('error', {
-    message: err.message,
-    error: {}
-  });
+response.render('error', {
+  message: err.message,
+  error: {}
+});
 });
 
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+  console.log(`Application worker ${process.pid} started...`);
+});
 
 const ImportData = require('./app/importdata');
 /*
-var mongoose = require('mongoose');
-var id = mongoose.Types.ObjectId('5471aad59c93d7dd010ce3c1');
-debug(id);
-*/
+ var mongoose = require('mongoose');
+ var id = mongoose.Types.ObjectId('5471aad59c93d7dd010ce3c1');
+ debug(id);
+ */
 ImportData.process();
 
 
-module.exports = app;
+//module.exports = app;
